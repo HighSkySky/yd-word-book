9c10155d82c10d24fdc26289831603139bb9af43
sanwu <highskysky97@gmail.com>
2019-11-16 16:31:54 +0800

    支持命令行

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log(1);
+#!/usr/bin/env node
+
+import commander from 'commander';
+import { searchWord } from './lib/search-word';
+import { toParallel } from './utils/toParallel';
+
+const program = new commander.Command();
+
+let useSubCommand = false;
+
+(() => {
+  let words: string[] = [];
+  program
+    .arguments('<word> [otherWords...]')
+    .action(async (word: string, otherWords?: string[]) => {
+      if (word) words.push(word);
+      if (otherWords) words = words.concat(otherWords);
+
+      if (!useSubCommand && words.length > 0) {
+        console.log(`🔍  Search word...`);
+        await toParallel(
+          words.map(w => () => searchWord(w)),
+          2
+        );
+      }
+    });
+})();
+
+program.parse(process.argv);
--- src/lib/search-word.ts
+++ src/lib/search-word.ts
@@ -4,7 +4,9 @@ import { search } from '../api/search';
 
 const parseText = (html: string) => {
   const texts = [];
-  const $ = cheerio.load(html);
+  const $ = cheerio.load(html, {
+    ignoreWhitespace: true
+  });
   texts.push(
     $('#phrsListTab .baav')
       .text()
@@ -13,15 +15,12 @@ const parseText = (html: string) => {
   $('#phrsListTab .trans-container li').each((_, ele) => {
     texts.push($(ele).text());
   });
-  return texts.join('\n');
+  return texts.filter(text => text.trim()).join('\n');
 };
 
 export const searchWord = async (word: string) => {
-  console.log(`🔍  Search word...`);
   const { data, config } = await search(word);
   console.log(`${colors.green('success')} Get '${word}' translate`);
   console.log(`${colors.blue('info')} See more form: ${config.url}`);
   console.log(parseText(data));
 };
-
-searchWord('book');
