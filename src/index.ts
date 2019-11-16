bb35a2170d0f01da086879844cb49ad519693ab3
sanwu <highskysky97@gmail.com>
2019-11-16 22:51:59 +0800

    publish first version 0.0.1

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,21 @@ const program = new commander.Command();
 
 let useSubCommand = false;
 
-(() => {
-  program
-    .arguments('<word> [otherWords...]')
-    .action(async (word: string, otherWords: string[] = []) => {
-      if (!useSubCommand) {
-        console.log(`🔍  Search word...`);
-        if (word) {
-          await searchWord(word);
-          await toParallel(
-            otherWords.map(w => () => searchWord(w)),
-            2
-          );
-        }
+program
+  .arguments('<word> [otherWords...]')
+  .action(async (word: string, otherWords: string[] = []) => {
+    if (!useSubCommand) {
+      console.log(`🔍  Search word...`);
+      if (word) {
+        await searchWord(word);
+        await toParallel(
+          otherWords.map(w => () => searchWord(w)),
+          2
+        );
       }
-    });
-})();
+    }
+  });
+
+program.version('0.0.1', '-v, --version', 'output the version number');
 
 program.parse(process.argv);
